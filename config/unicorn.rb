e92dc31639993b129cf3e32ab28e0dec750d4b16
Seiya <tripworld1001@gmail.com>
2016-10-14 18:20:09 +0900

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,32 +1,27 @@
 # -*- coding: utf-8 -*-
 APP_PATH = "~/workspace/CableChat"
 worker_processes 1
-timeout 15
-preload_app true  # 更新時ダウンタイム無し
-
+working_directory APP_PATH
 listen "/tmp/unicorn.sock"
-pid "/tmp/unicorn.pid"
+pid APP_PATH + "/tmp/pids/unicorn.pid"
+stderr_path APP_PATH + "/log/unicorn.stderr.log"
+stdout_path APP_PATH + "/log/unicorn.stdout.log"
+
+preload_app true
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
   end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  sleep 1
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
-
-# ログの出力
-stderr_path APP_PATH + "/log/unicorn_err.log"
-stdout_path APP_PATH + "/log/unicorn.log"
-

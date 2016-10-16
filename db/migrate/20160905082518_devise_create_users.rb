6782775c2d30640862ac8066224903a313e45e79
Seiya <tripworld1001@gmail.com>
2016-10-16 22:42:21 +0900

    email

--- db/migrate/20160905082518_devise_create_users.rb
+++ db/migrate/20160905082518_devise_create_users.rb
@@ -3,10 +3,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-   
-      t.string :email,              null: false, default: ""
+
+      t.string :email,              null: false, default: "", limit: 191
       t.string :encrypted_password, null: false, default: ""
-    
+
 
       ## Recoverable
       t.string   :reset_password_token

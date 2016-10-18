5707f7e7680582b5e1aee302e86b7307ec1faeab
Seiya <tripworld1001@gmail.com>
2016-10-18 10:20:14 +0900

    limit

--- db/migrate/20160905082518_devise_create_users.rb
+++ db/migrate/20160905082518_devise_create_users.rb
@@ -5,11 +5,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
 
       t.string :email,              null: false, default: "", limit: 191
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: "", limit: 191
 
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string   :reset_password_token, limit: 191
       t.datetime :reset_password_sent_at
 
       ## Rememberable
